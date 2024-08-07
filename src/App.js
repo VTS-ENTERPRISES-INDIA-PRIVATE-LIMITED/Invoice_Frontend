3e2f2ec022def10cb4fa7357d885cb974eb2bf75
VenkatasaiNagendraNaidu <venkatasainagedranaidualthi@gmail.com>
2024-08-07 23:52:25 +0530

    grhtykjhgrtyukjytghyjhggfg

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import Forgot from './Components/Forgot';
+import Formm from './Components/Formm'
 import './App.css';
+import LandingPage from './Components/LandingPage';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login/>} />
-        <Route path="/forgot" element={<Forgot/>} />
+      <Route path='/' element={<LandingPage/>}/>
+      <Route path="/login" element={<Login/>} />
+      <Route path="/new" element={<Formm/>} />
+      <Route path="/forgot" element={<Forgot/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
     </Routes>
     </BrowserRouter>
   );
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Navigate, useNavigate } from "react-router-dom";
 import {
   AppstoreOutlined,
   ContainerOutlined,
@@ -8,7 +9,7 @@ import {
   MenuUnfoldOutlined,
   PieChartOutlined,
 } from '@ant-design/icons';
-import { Button, Menu } from 'antd';
+import { Button, Menu,Dropdown } from 'antd';
 const items = [
   {
     key: '1',
@@ -29,39 +30,48 @@ const items = [
 
 
 const Dashboard = () => {
+  const navigate=useNavigate()
+
+  
         const [collapsed, setCollapsed] = useState(false);
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
   return (
-    <>
-        <div
-      style={{
-        width: "20vw"
-      }}
-    >
-      <Button
-        type="primary"
-        onClick={toggleCollapsed}
-        style={{
-          marginBottom: 16,
-        }}
-      >
+    <div style={{display:"flex"}}>
+      <div style={{width: "20vw",height:"95vh",backgroundColor:"rgb(0, 21, 41)"}}>
+        <Button type="primary" onClick={toggleCollapsed}
+          style={{marginTop: 16}}
+        >
         {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-      </Button>
-      <Menu
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub1']}
-        mode="inline"
-        theme="light"
-        inlineCollapsed={collapsed}
-        items={items}
-      />
-    </div>
+        </Button>
+        <Menu style={{marginTop: 16,borderRadius:"5px"}}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={collapsed}
+          items={items}
+        />
+    </div >
+    {/* Invoices */}
+    {/* <div style={{marginLeft:"10px",marginTop:"10px",fontSize:"20px"}}>
+        <div style={{display:"flex"}}>
+          <p>Recent Invoices</p>
+          <Button style={{marginLeft:"10px"}}  onClick={()=>navigate('/new')} type="primary">+ New</Button>
+        </div>
+        <div style={{display:"flex",height:"20vh", width:"78vw",justifyContent:"center",alignItems:"center"}}>
+          <p>Nothing to see here yet. Click 'New' above to get started!</p>
+        </div>
+        <hr />
+        <div>
+          <p style={{fontSize:"30px"}}>All invoices</p>
+        </div>
+    </div> */}
     <div>
-        <p>Recent Invoices</p>
+      <p>Clients</p>
     </div>
-    </>
+    </div>
   );
 };
 export default Dashboard
